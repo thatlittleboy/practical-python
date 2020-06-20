0feeb62e15165892cda995b2aa117afbe6eab61c
Jeremy Goh <jeremy_gsk@outlook.com>
2020-06-20 17:14:26 +0800

    UPD: convert ex 1.8 into a function

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,16 +1,18 @@
 # mortgage.py
 #
 # Exercise 1.8
-principal = 500_000.
-rate = 0.05
-payment = 2684.11
-total_paid = 0.
-num_month = 0
+def mortgage_calculator_ex_1_8():
+    principal = 500_000.
+    rate = 0.05
+    payment = 2684.11
+    total_paid = 0.
+    num_month = 0
 
-while principal > 0:
-    num_month += 1
-    extra_payment = 1_000 if num_month <= 12 else 0
-    principal = principal * (1 + rate / 12) - payment - extra_payment
-    total_paid = total_paid + payment - extra_payment
+    while principal > 0:
+        num_month += 1
+        extra_payment = 1_000 if num_month <= 12 else 0
+        principal = principal * (1 + rate / 12) - payment - extra_payment
+        total_paid = total_paid + payment - extra_payment
 
-print(f"Total paid {total_paid:.1f} over {num_month} months.")
+    print(f"Total paid {total_paid:.1f} over {num_month} months.")
+
