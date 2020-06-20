1469a5a4d3fcb591a6f8861b03dc9465229ebc7e
Jeremy Goh <jeremy_gsk@outlook.com>
2020-06-20 17:15:54 +0800

    FIX: bug with total payment

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -12,7 +12,8 @@ def mortgage_calculator_ex_1_8():
         num_month += 1
         extra_payment = 1_000 if num_month <= 12 else 0
         principal = principal * (1 + rate / 12) - payment - extra_payment
-        total_paid = total_paid + payment - extra_payment
+        total_paid = total_paid + payment + extra_payment
 
     print(f"Total paid {total_paid:.1f} over {num_month} months.")
+
 
