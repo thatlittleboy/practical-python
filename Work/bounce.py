1767d8c3f57ccb6a4985a63fe751160fdd6dcec4
Jeremy Goh <jeremy_gsk@outlook.com>
2020-06-20 16:56:04 +0800

    ADD: completed bounce

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+def bounce_height(initial_height, num_bounces):
+    height = initial_height
+    for i in range(1, num_bounces + 1):
+        height = height * 0.6
+        print(f"{i} {round(height, 4)}")
+
+
+if __name__ == "__main__":
+    bounce_height(100, num_bounces=10)
