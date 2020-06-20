48b9aef06e1df9c22c47b3ef01eadd7ba944ac39
Jeremy Goh <jeremy_gsk@outlook.com>
2020-06-20 16:59:26 +0800

    UPD: Ex 1.7

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,13 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500_000.
+rate = 0.05
+payment = 2684.11
+total_paid = 0.
+
+while principal > 0:
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+
+print(f"Total paid {total_paid:.1f}")
